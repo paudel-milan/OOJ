cd03c5e9c39a99f753c429b9111e71c1334ebb27
paudel-milan <milanpaudel12kk@gmail.com>
2024-11-02 21:33:41 +0530

    Update sgpa.java

--- sgpa.java
+++ sgpa.java
@@ -1,56 +1,69 @@
 import java.util.Scanner;
 class Stud_details{
-int marks[]=new int[3];
-int credits[]=new int[3];
-String usn,name;
-Scanner sc=new Scanner(System.in);
+    int n;
+    int marks[];
+    int credits[];
+    String usn,name;
+    Scanner sc=new Scanner(System.in);
 
-void getDetails(){
-System.out.println("Enter the USN:")
-usn=sc.next();
-System.out.println("Enter the Name:");
-name=sc.next();
-SYstem.out.println("Enter the credits and marks of 3 subjects: ");
-for(int i=0;i<3;i++){
-credits[i]=sc.nextInt();
-marks[i]=sc.nextInt();
-}
+    public Stud_details() {
+        System.out.println("Enter the number of subjects: ");
+        n=sc.nextInt();
+        marks=new int[n];
+        credits=new int[n];
+    }
+
+    
+
+
+    void getDetails(){
+        System.out.println("Enter the USN:");
+        usn=sc.next();
+
+        sc.nextLine();
+        System.out.println("Enter the Name:");
+        name=sc.nextLine();
+        System.out.println("Enter the credits and marks of"+" n " + "subjects: ");
+        for(int i=0;i<n;i++){
+            credits[i]=sc.nextInt();
+            marks[i]=sc.nextInt();
+        }
+    }
+
+    void displayDetails(){
+        System.out.println("Name: "+name+" USN: "+usn);
+        System.out.println("The marks and credits of "+n+" subjects: ");
+        for(int i=0;i<n;i++){
+            System.out.println(marks[i]+":"+credits[i]);
+        }
+        System.out.println("SGPA: "+calculateSGPA());
+    }
+    public double calculateSGPA() {
+        double totalCredits = 0;
+        double totalPoints = 0;
+
+        for (int i = 0; i < n; i++) {
+            double points = 0;
+            if (marks[i] >= 90) points = 10;
+            else if (marks[i] >= 80) points = 9;
+            else if (marks[i] >= 70) points = 8;
+            else if (marks[i] >= 60) points = 7;
+            else if (marks[i] >= 50) points = 6;
+            else if (marks[i] >= 40) points = 5;
+            totalCredits += credits[i];
+            totalPoints += points * credits[i];
+            }
+
+        return totalPoints/totalCredits;
+        }
 }
 
-void displayDetails{
-System.out.println("Name: "+name+" USN: "+usn);
-System.out.println("The marks and credits of 3 subjects: ");
-for(int i=0;i<3;i++){
-System.out.println(marks[i]+":"+credits[i];
-System.out.println("SGPA: "+calculateSGPA());
-}
-}
-public double calculateSGPA() {
-double totalCredits = 0;
-double totalPoints = 0;
+class sgpa{
+    public static void main(String[] args){
+    Stud_details s1=new Stud_details();
+    s1.getDetails();
+    s1.displayDetails();
 
-for (int i = 0; i < 3; i++) {
-double points = 0;
-if (marks[i] >= 90) points = 10;
-else if (marks[i] >= 80) points = 9;
-else if (marks[i] >= 70) points = 8;
-else if (marks[i] >= 60) points = 7;
-else if (marks[i] >= 50) points = 6;
-else if (marks[i] >= 40) points = 5;
-totalCredits += credits[i];
-totalPoints += points * credits[i];
-}
-
-return totalPoints/totalCredits;
-}
-}
-
-class Student{
-public static void main(String[] args){
-Stud_details s1=new Stud_details[3];
-s1.getDetails();
-s1.displayDetails();
-
-}  
+    }  
 
 }
